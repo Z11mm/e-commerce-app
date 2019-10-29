e16e77d2d6a9a073c0bfd0a543addf0204a2af6f
Ghad <zimobie@gmail.com>
2019-10-29 07:16:17 +0100

    e-commerce homepage and sass setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import HomePage from './Homepage';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
