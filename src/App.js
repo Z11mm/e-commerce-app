f1ede395b9e2755e514b7b1c1372fd4c282c2998
Ghad <zimobie@gmail.com>
2019-11-01 20:46:05 +0100

    shop page folder and collection preview component created

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import HomePage from './pages/homepage/Homepage';
+import ShopPage from './pages/shop/Shop';
 import './App.css';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route path="/shop/hats" component={HatsPage} />
+      <Route path="/shop" component={ShopPage} />
     </div>
   );
 }
