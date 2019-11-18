0831304ebaa59feaeeb9e6a5eac692b24835a3ed
Ghad <zimobie@gmail.com>
2019-11-18 09:27:56 +0100

    setting redux for create react app

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/app/App';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  </Provider>
   document.getElementById('root')
 );
