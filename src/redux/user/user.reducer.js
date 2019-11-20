626d8eefd567af1abaaf56c31086cd3a1d5e25a0
Ghad <zimobie@gmail.com>
2019-11-20 10:06:05 +0100

    setting up redux for create react app

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,7 +1,17 @@
 const INITIAL_STATE = {
   currentUser: null
-}
+};
 
-const userReducer = (state, action) => {
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'SET_CURRENT_USER':
+      return {
+        ...state,
+        currentUser: action.payload
+      };
+    default:
+      return state;
+  }
+};
 
-}
+export default userReducer;
