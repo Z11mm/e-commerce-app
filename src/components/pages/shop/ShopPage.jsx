08488d74e7d8ec8b178a6a7658cfee6c0b3ef24f
Ghad <zimobie@gmail.com>
2019-11-29 21:47:01 +0100

    updated cart utils to group items in cart reducer

--- src/components/pages/shop/ShopPage.jsx
+++ src/components/pages/shop/ShopPage.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import CollectionPreview from '../../preview-collection/CollectionPreview';
-import SHOP_DATA from './shop.data';
+import React from "react";
+import CollectionPreview from "../../preview-collection/CollectionPreview";
+import SHOP_DATA from "./shop.data";
 
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      collections: SHOP_DATA,
-    }
+      collections: SHOP_DATA
+    };
   }
 
   render() {
     const { collections } = this.state;
     return (
       <div className="shop-page">
-        {
-          collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-          ))
-        }
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
       </div>
     );
   }
--- src/components/preview-collection/CollectionPreview.jsx
+++ src/components/preview-collection/CollectionPreview.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import CollectionItem from '../collection-item/CollectionItem';
-import './collection-preview.styles.scss';
+import React from "react";
+import CollectionItem from "../collection-item/CollectionItem";
+import "./collection-preview.styles.scss";
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
