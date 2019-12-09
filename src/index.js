ef6c9c09ffb24bb37bd8eed851663c300024d8b7
Ghad <zimobie@gmail.com>
2019-12-09 10:46:55 +0100

    set up persistence in local storage using redux-persist

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
+/* eslint-disable import/named */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable react/jsx-filename-extension */
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
+
 import "./index.css";
+
 import App from "./components/app/App";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
