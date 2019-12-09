64409b861f131e4fd8b36e7aaced4f15bd07e574
Ghad <zimobie@gmail.com>
2019-12-09 11:30:02 +0100

    moved state in Directory component into redux and changed Directory into a functional component

--- src/components/pages/homepage/Homepage.jsx
+++ src/components/pages/homepage/Homepage.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Directory from '../../directory/Directory';
-import './homepage.styles.scss';
+import React from "react";
+import Directory from "../../directory/Directory";
+import "./homepage.styles.scss";
 
 const HomePage = () => (
   <div className="homepage">
