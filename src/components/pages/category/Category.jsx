b2b87727d58ca6dbacf0152f1f6f03b31645ace1
Ghad <zimobie@gmail.com>
2019-12-13 22:16:10 +0100

    created a Category component and implementing routing on the ShopPage for category collections

---
+++ src/components/pages/category/Category.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./category.styles.scss";
+
+const CategoryPage = () => (
+  <div className="category">
+    <h2>CATEGORY PAGE</h2>
+  </div>
+);
+
+export default CategoryPage;
--- src/components/pages/shop/ShopPage.jsx
+++ src/components/pages/shop/ShopPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import CollectionOverview from "../../collection-overview/CollectionOverview";
+import CategoryPage from "../category/Category";
 
-const ShopPage = () => {
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      <CollectionOverview />
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
     </div>
   );
 };
