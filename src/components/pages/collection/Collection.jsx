f28aa8ed4a06279326eafe93f38f03ffcfb54588
Ghad <zimobie@gmail.com>
2019-12-16 11:31:56 +0100

    created Collection selector and implemented routing to Collection page from Directory

--- src/components/pages/collection/Collection.jsx
+++ src/components/pages/collection/Collection.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import { selectCollection } from "../../../redux/shop/shoppage.selectors";
+import CollectionItem from "../../collection-item/CollectionItem";
+
 import "./collection.styles.scss";
 
-const CollectionPage = () => (
-  <div className="collection-page">
-    <h2>COLLECTION PAGE</h2>
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/redux/shop/shoppage.selectors.js
+++ src/redux/shop/shoppage.selectors.js
@@ -1,8 +1,23 @@
 import { createSelector } from "reselect";
 
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5
+};
+
 const selectShopPage = state => state.shoppage;
 
 export const selectShopPageCollections = createSelector(
   [selectShopPage],
   shoppage => shoppage.collections
 );
+
+export const selectCollection = collectionUrlParam =>
+  createSelector([selectShopPageCollections], collections =>
+    collections.find(
+      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    )
+  );
