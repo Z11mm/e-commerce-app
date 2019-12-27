582677b51c8c3dec083e193f8efd2ae3301af316
Ghad <zimobie@gmail.com>
2019-12-27 12:11:51 +0100

    corrected add-to-cart button placement

--- src/components/custom-button/CustomButton.jsx
+++ src/components/custom-button/CustomButton.jsx
@@ -1,10 +1,22 @@
 /* eslint-disable react/button-has-type */
 import React from "react";
 
-import { CustomButtonContainer } from "./CustomButtonStyles";
+import "./custom-button.styles.scss";
 
-const CustomButton = ({ children, ...props }) => (
-  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
+  <button
+    className={`${inverted ? "inverted" : ""} ${
+      isGoogleSignIn ? "google-sign-in" : ""
+    } custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
 );
 
 export default CustomButton;
