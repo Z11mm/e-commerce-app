65d4dacf614e7529de16ad4136e55a1d58b2f2a7
Ghad <zimobie@gmail.com>
2020-01-15 11:13:11 +0100

    implemented async handling of actions using redux-saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,7 @@ import logger from "redux-logger";
 import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./root-reducer";
+import rootSaga from "./root-saga";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -16,6 +17,6 @@ if (process.env.NODE_ENV === "development") {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run();
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
