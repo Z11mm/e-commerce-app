b4384ec8146206b179b776a61aa12ef622625529
Ghad <zimobie@gmail.com>
2020-02-21 12:32:32 +0100

    created route to handle payment requests to Stripe

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require("path");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -23,5 +25,21 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(port, error => {
   if (error) throw error;
-  console.log('Server running on' + port);
-})
+  console.log("Server running on" + port);
+});
+
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "usd"
+  };
+
+  stripe.charges.create(body, (stripeError, stripeResponse) => {
+    if (stripeError) {
+      res.status(500).send({ error: stripeError });
+    } else {
+      res.status(200).send({ success: stripeResponse });
+    }
+  });
+});
