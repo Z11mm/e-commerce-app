e9a736754d943b648522c45a9b15bf3759fe5d4f
Ghad <zimobie@gmail.com>
2020-02-21 11:40:16 +0100

    added port in server and added proxy address in client directory

--- server.js
+++ server.js
@@ -4,3 +4,6 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
