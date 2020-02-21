6cd4e5a573e4f7bf184669634dca6588a3e5fd0a
Ghad <zimobie@gmail.com>
2020-02-21 11:58:32 +0100

    set up server boilerplate

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", function(req, res) {
+    res.sendFile(__dirname, "client/build", "index.html");
+  });
+}
+
+app.listen(port, error => {
+  if (error) throw error;
+  console.log('Server running on' + port);
+})
