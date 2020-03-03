8aa13cd3e3dbafcf3c283317af81dffeac40ca2f
Ghad <zimobie@gmail.com>
2020-03-03 07:37:09 +0100

    removed service worker request

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.listen(port, error => {
   console.log("Server running on " + port);
 });
 
-app.get("/service-worker.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
-});
+// app.get("/service-worker.js", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
+// });
 
 app.post("/payment", (req, res) => {
   const body = {
