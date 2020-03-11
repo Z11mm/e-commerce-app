280a758f92dd8013a370f6099512a4b5b5271d7a
Ghad <zimobie@gmail.com>
2020-03-11 19:50:49 +0100

    modified homepage styles and added currency symbol to price

--- client/src/components/menu-item/MenuItemStyles.jsx
+++ client/src/components/menu-item/MenuItemStyles.jsx
@@ -8,7 +8,6 @@ export const MenuItemContainer = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	border: 1px solid black;
 	margin: 0 7.5px 15px;
 	overflow: hidden;
 	&:hover {
@@ -47,7 +46,6 @@ export const ContentContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  border: 1px solid black;
   background-color: white;
   opacity: 0.7;
   position: absolute;
