83daa9383e75486b1d24510e5ca8a8082788d94e
Ghad <zimobie@gmail.com>
2020-03-25 16:36:13 +0100

    refactored code to only display add-to-cart button in product details page

--- client/src/components/pages/product-details/ProductInfo.jsx
+++ client/src/components/pages/product-details/ProductInfo.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
 
-import { selectCartItems } from "../../../redux/cart/cart.selectors";
 import { addItemToCart, removeItem } from "../../../redux/cart/cart.actions";
 
 import { AddButton } from "./ProductDetailsStyles";
 
-const ProductInfo = ({ item, addItem, removeItem, cartItems }) => {
-  console.log(cartItems)
+const ProductInfo = ({ item, addItem }) => {
+  // console.log(cartItems)
   const { name, price } = item;
   // const cartItem = cartItems.find(cartItem => cartItem.id === item.id)
   
@@ -21,27 +19,14 @@ const ProductInfo = ({ item, addItem, removeItem, cartItems }) => {
         elit. Quis, fugit.
       </p>
       <p className="price">${price}</p>
-      <span className="quantity">
-        <div className="arrow" onClick={() => removeItem(item)}>
-          &#10094;
-        </div>
-        {/* <span className="value">{quantity}</span> */}
-        <div className="arrow" onClick={() => addItemToCart(item)}>
-          &#10095;
-        </div>
-      </span>
       <AddButton onClick={() => addItem(item)}>Add To Cart</AddButton>
     </div>
   );
 };
-
-const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems
-});
 
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItemToCart(item)),
   removeItem: item => dispatch(removeItem(item))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProductInfo);
+export default connect(null, mapDispatchToProps)(ProductInfo);
