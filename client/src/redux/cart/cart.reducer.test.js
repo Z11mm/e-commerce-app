243c1858f28f4e299db9964167ced27b435f082f
Ghad <zimobie@gmail.com>
2020-04-02 21:54:16 +0100

    adding tests for cartReducer

--- client/src/redux/cart/cart.reducer.test.js
+++ client/src/redux/cart/cart.reducer.test.js
@@ -1,3 +1,24 @@
-import React from 'react'
+import React from "react";
 
-import cartReducer from './cart.reducer'
+import CartActionTypes from "./cart.types";
+import cartReducer from "./cart.reducer";
+
+const initialState = {
+  hidden: true,
+  cartItems: []
+};
+
+describe("cartReducer", () => {
+  it("should return initial state", () => {
+    expect(cartReducer(undefined, {})).toEqual(initialState);
+  });
+
+  it("should hide dropdown on toggle hidden", () => {
+    expect(
+      cartReducer(initialState, { type: CartActionTypes.TOGGLE_CART_DROPDOWN })
+        .hidden
+    ).toBe(false);
+  });
+
+  it()
+});
