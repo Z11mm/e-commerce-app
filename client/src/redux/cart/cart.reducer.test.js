b2c683ba6c64a7b3130449640e22362e72765723
Ghad <zimobie@gmail.com>
2020-04-03 20:20:03 +0100

    added tests for cart reducer

--- client/src/redux/cart/cart.reducer.test.js
+++ client/src/redux/cart/cart.reducer.test.js
@@ -20,5 +20,76 @@ describe("cartReducer", () => {
     ).toBe(false);
   });
 
-  it()
+  it("should increment quantity of matching item by 1 if addItemToCart action fired with same item as payload", () => {
+    const mockItem = {
+      id: 1,
+      quantity: 3
+    };
+
+    const mockPrevState = {
+      hidden: true,
+      cartItems: [mockItem, { id: 2, quantity: 1 }]
+    };
+
+    expect(
+      cartReducer(mockPrevState, {
+        type: CartActionTypes.ADD_ITEM_TO_CART,
+        payload: mockItem
+      }).cartItems[0].quantity
+    ).toBe(4);
+  });
+
+  it("should decrease quantity of matching item by 1 if removeItem action fired with same item as payload", () => {
+    const mockItem = {
+      id: 1,
+      quantity: 3
+    };
+
+    const mockPrevState = {
+      hidden: true,
+      cartItems: [mockItem, { id: 2, quantity: 1 }]
+    };
+
+    expect(
+      cartReducer(mockPrevState, {
+        type: CartActionTypes.REMOVE_ITEM,
+        payload: mockItem
+      }).cartItems[0].quantity
+    ).toBe(2);
+  });
+
+  it("should remove item from cart if clearItemFromCart action fired with payload of existing item", () => {
+    const mockItem = {
+      id: 1,
+      quantity: 3
+    };
+
+    const mockPrevState = {
+      hidden: true,
+      cartItems: [mockItem, { id: 2, quantity: 1 }]
+    };
+
+    expect(
+      cartReducer(mockPrevState, {
+        type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+        payload: mockItem
+      }).cartItems.includes(item => item.id === 1)
+    ).toBe(false);
+  });
+
+  it("should clear cart if clearCart action fired", () => {
+    const mockPrevState = {
+      hidden: true,
+      cartItems: [
+        { id: 1, quantity: 3 },
+        { id: 2, quantity: 1 }
+      ]
+    };
+
+    expect(
+      cartReducer(mockPrevState, {
+        type: CartActionTypes.CLEAR_CART
+      }).cartItems.length
+    ).toBe(0);
+  });
 });
